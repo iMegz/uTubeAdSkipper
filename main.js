b8e35ae473df13acb87d5fe70061b3178efdb427
Ahmed Magdi <ammhaa2012@gmail.com>
2024-06-08 13:58:35 +0300

    Temporary fix for youtube detection

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 const observer = new MutationObserver((mutationsList, observer) => {
-    for (const mutation of mutationsList) {
-        if (mutation.target.classList.contains("ytp-ad-text")) {
-            const video = document.querySelector("video");
-            if(video) video.currentTime = video.duration;
-            
-            const skipBtn = document.querySelector(".ytp-ad-skip-button");
-            const skipBtnModern = document.querySelector(".ytp-ad-skip-button-modern");
-            if(skipBtn) skipBtn.click();
-            if(skipBtnModern) skipBtnModern.click();
-            
-        }
+  for (const mutation of mutationsList) {
+    if (mutation.target.classList.contains("ytp-ad-text")) {
+      const video = document.querySelector("video");
+      if (video) video.currentTime = video.duration;
+
+      // const skipBtn = document.querySelector(".ytp-ad-skip-button");
+      // const skipBtnModern = document.querySelector(".ytp-ad-skip-button-modern");
+      // if(skipBtn) skipBtn.click();
+      // if(skipBtnModern) skipBtnModern.click();
     }
+  }
 });
 
 const observerOptions = { childList: true, subtree: true };
